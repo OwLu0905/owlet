e2827d4878b210afee402d6da9ff2862818e9050
OwLu0905 <owlu0905@gmail.com>
2025-03-14 15:33:46 +0800

    mark 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,36 @@
 <script lang="ts">
-	import Button from "$lib/components/ui/button/button.svelte";
-	import { Input } from "$lib/components/ui/input";
-	import { invoke } from "@tauri-apps/api/core";
+  import Button from "$lib/components/ui/button/button.svelte";
+  import { Input } from "$lib/components/ui/input";
+  import { invoke } from "@tauri-apps/api/core";
+  import * as Card from "$lib/components/ui/card";
+  let name = $state("");
+  let greetMsg = $state("");
 
-	let name = $state("");
-	let greetMsg = $state("");
-
-	async function greet(event: Event) {
-		event.preventDefault();
-		// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-		greetMsg = await invoke("greet", { name });
-	}
+  async function greet(event: Event) {
+    event.preventDefault();
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsg = await invoke("greet", { name });
+  }
 </script>
 
 <main class="container p-20">
-	<h1>Welcome to Tauri + Svelte</h1>
+  <Card.Root>
+    <Card.Header>
+      <h1>Welcome to Tauri + Svelte</h1>
+    </Card.Header>
 
-	<form class="row" onsubmit={greet}>
-		<div class="flex gap-4">
-			<Input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-			<Button type="submit">Greet</Button>
-		</div>
-	</form>
-	<p>{greetMsg}</p>
+    <Card.Content>
+      <form class="row" onsubmit={greet}>
+        <div class="flex gap-4">
+          <Input
+            id="greet-input"
+            placeholder="Enter a name..."
+            bind:value={name}
+          />
+          <Button type="submit">Greet</Button>
+        </div>
+        <p>{greetMsg}</p>
+      </form>
+    </Card.Content>
+  </Card.Root>
 </main>
